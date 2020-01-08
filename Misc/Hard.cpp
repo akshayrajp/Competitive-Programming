adabd2ea81682d007cd86e7f5e40a552846b31d0
Akshay Rajpurohit <36583678+akshayrajp@users.noreply.github.com>
2020-01-09 00:48:49 +0530

    Final commit

--- Misc/Hard.cpp
+++ Misc/Hard.cpp
@@ -1,6 +1,12 @@
+/*
+Akshay K Rajpurohit
+17P61A0405
+Unique Code: 8504
+*/
 #include <iostream>
 using namespace std;
-bool isPrime(long long int n)
+// Function used to check if the given input is a prime number or not.
+bool isPrime(int n)
 {
 	if (n <= 1)
 		return false;
@@ -16,53 +22,30 @@ bool isPrime(long long int n)
 int main()
 {
 	int t, w1, w2, w3, e;
+	/* 
+	t - Number of test cases.
+	w1,w2,w3 - Energy required for each of the three Ways respectively.
+	e - Energy that the user has.
+	*/
 	cin >> t;
 	while (t--)
 	{
 		cin >> w1 >> w2 >> w3 >> e;
-		if (e < w1 && e < w2 && e < w3)
+		// Two conditions where the answer is definitely NO:
+		// First is if the energy that the user has is lesser than the energy required by each of the three ways.
+		// Second is if all the three ways are prime valued.
+		if (e < w1 && e < w2 && e < w3 || isPrime(w1) && isPrime(w2) && isPrime(w3))
 		{
 			cout << "NO" << endl;
 			continue;
 		}
-		if (isPrime(w1) && isPrime(w2) && isPrime(w3))
-		{
+		// With the extreme test cases aka corner test cases out of our way, we can now focus on the general conditions.
+		// Either of the three paths have to satisy both conditions :
+		// being composite and requiring energy <= energy of the user.
+		if (w1 <= e && !isPrime(w1) || w2 <= e && !isPrime(w2) || w3 <= e && !isPrime(w3))
+			cout << "YES" << endl;
+		else
 			cout << "NO" << endl;
-			continue;
-		}
-		if (!isPrime(w1) && !isPrime(w2) && !isPrime(w3))
-		{
-			if (w1 <= e || w2 <= e || w3 <= e)
-				cout << "YES" << endl;
-			else
-				cout << "NO" << endl;
-			continue;
-		}
-
-		if (isPrime(w1))
-		{
-			if (!isPrime(w2) && w2 <= e ||
-				!isPrime(w3) && w3 <= e)
-				cout << "YES" << endl;
-			else
-				cout << "NO" << endl;
-		}
-		else if (isPrime(w2))
-		{
-			if (!isPrime(w1) && w1 <= e ||
-				!isPrime(w3) && w3 <= e)
-				cout << "YES" << endl;
-			else
-				cout << "NO" << endl;
-		}
-		else if (isPrime(w3))
-		{
-			if (!isPrime(w1) && w1 <= e ||
-				!isPrime(w2) && w2 <= e)
-				cout << "YES" << endl;
-			else
-				cout << "NO" << endl;
-		}
 	}
 	return 0;
 }
