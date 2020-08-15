08a34f426c9639f23daeda3ce2fee6d92cc946b1
Akshay Rajpurohit <akj.rajpurohit@gmail.com>
2020-08-15 21:11:53 +0530

    Nice

--- CodeChef/ZCO12001.cpp
+++ CodeChef/ZCO12001.cpp
@@ -1,80 +1,58 @@
 #include <iostream>
-#include <utility>
-#include <algorithm>
-#include <stack>
 using namespace std;
 int main()
 {
-	int n,i,c=0,max_nest = 0, max_nest_pos=0,max_bracket = 0,max_bracket_pos =0;
-	stack<int> brackets;
+	int n, i, nesting_depth = 0, nesting_depth_pos = 0, max_sequence = 0, max_sequence_pos = 0;
+	int bracket, start_pos = 0, bracketsOpen = 0, sequenceLength = 0;
 	cin >> n;
-	int arr[n];
-	for(i = 0; i < n; i++)
-		cin >> arr[i];
-	// Maximum nesting depth and pos
-	for(i = 0; i < n; i++)
-	{
-		if(arr[i] == 1)
-		{
-			while(arr[i] == 1)
-				c++,i++;
-			max_nest = max(c,max_nest);
-			c = 0;
-		}
-	}
-	cout << max_nest << " ";
-	for(i = 0; i < n; i++)
-	{
-		if(arr[i] == 1)
-		{
-			while(arr[i] == 1)
-				c++,i++;	
-		}
-		if(c == max_nest)
-		{
-			max_nest_pos = i;
-			break;
-		}
-		c = 0;
-	}
-	cout << max_nest_pos << " ";
 
-	// Longest bracket sequence
-	for(i = 0; i<n; i++)
-	{
-		if(arr[i] == 1)
-			brackets.push(1);
-		else
-			brackets.pop();
-		if(!brackets.empty())
-			c++;
-		else
-		{
-			max_bracket = max(c+1,max_bracket);
-			c = 0;
-		}
-	}
-	cout << max_bracket << " ";
+	/* 
+	 * The algorithm used here is as follows:
+	 * We start by accepting a bracket input.
+	 * If the bracket is '(', then we set the value of start to the value of i, to indicate start.
+	 * We then increment the value of bracketsOpen and sequenceLength
+	 * If the value of bracketsOpen > nesting_depth, then nesting_depth = bracketsOpen
+	 * Else if the bracket is ')', decrement the value of bracketsOpen and increment the length of sequenceLength
+	 * If the value of bracketsOpen is 0, indicating that all brackets have been closed,
+	 * 	check if the value of sequenceLength is greater than max_sequence
+	 * 		If yes, max_sequence = sequenceLength and max_sequence_pos = start_pos
+	 * If the value of bracketsOpen is 0, reset the value of sequenceLength to 0.
+	 */
 
-	for(i = 0; i<n; i++)
+	for(i = 1; i <= n; i++)
 	{
-		if(arr[i] == 1)
-			brackets.push(1);
-		else
-			brackets.pop();
-		if(!brackets.empty())
-			c++;
-		else
+		// Accept the bracket input
+		cin >> bracket;
+		if(bracket == 1) // If the bracket is '('
 		{
-			if(c+1 == max_bracket)
+			if(sequenceLength == 0)
 			{
-				max_bracket_pos = (i+2) - max_bracket;
-				break;
+				start_pos = i;
 			}
-			c = 0;
+			sequenceLength ++;
+			bracketsOpen ++;
+			if(bracketsOpen > nesting_depth)
+			{
+				nesting_depth = bracketsOpen;
+				nesting_depth_pos = i;
+			}
+		}
+		else // If the bracket is ')'
+		{
+			bracketsOpen --;
+			sequenceLength++;
+			if(bracketsOpen == 0) // Indicating that all sequences have been closed
+			{
+				if(sequenceLength > max_sequence)
+				{
+					max_sequence = sequenceLength;
+					max_sequence_pos = start_pos;
+				}
+				sequenceLength = 0;
+			}
 		}
 
 	}
-	cout << max_bracket_pos;
+	cout << nesting_depth << " " << nesting_depth_pos << " " << max_sequence << " " << max_sequence_pos << '\n';
 	return 0;
 }
