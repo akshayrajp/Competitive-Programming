a93148d06e36d74c397e7410a2e01f1284df80c4
Akshay Rajpurohit <akj.rajpurohit@gmail.com>
2021-07-31 03:11:45 +0530

    final tidy

---
+++ Misc/size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	char arr[10];
+	int a;
+	long int b;
+
+	printf("Size of char[10] %d\n", sizeof(arr));
+
+	printf("Size of int %d\n", sizeof(a));
+
+	printf("Size of long int %d\n", sizeof(b));
+
+	return 0;
+}
+
